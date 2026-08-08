81d48e863e7276628ad316f7856695ea7dd46d94
epickalevaphoto-cyber <epickaleva.photo@gmail.com>
2026-08-08 11:01:47 +0300

    Refactor Player component to remove React import

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { LogOut, BookOpen, Sparkles, FlaskConical, Dice5, Backpack, Settings } from 'lucide-react';
 import Button from '../components/ui/Button';
 
-const Player: React.FC = () => {
+const Player = () => {
   const navigate = useNavigate();
   return (
     <div className="flex flex-col h-screen bg-krem">
