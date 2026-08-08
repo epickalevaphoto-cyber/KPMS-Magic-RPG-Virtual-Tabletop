175a6144e76a443babd2d6026346c42ad8de05e9
epickalevaphoto-cyber <epickaleva.photo@gmail.com>
2026-08-08 10:49:57 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,22 @@
-.
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import RootLayout from './layouts/RootLayout';
+import Home from './pages/Home';
+import Player from './pages/Player';
+import Master from './pages/Master';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RootLayout />}>
+          <Route index element={<Home />} />
+          <Route path="player" element={<Player />} />
+          <Route path="master" element={<Master />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
