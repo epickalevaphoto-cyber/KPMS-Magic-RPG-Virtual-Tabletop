489f14b48a0a30a11c148f6a014268df1f351f24
epickalevaphoto-cyber <epickaleva.photo@gmail.com>
2026-08-08 10:50:51 +0300

    Update RootLayout.tsx

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,1 +1,12 @@
-.
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+const RootLayout: React.FC = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Outlet />
+    </div>
+  );
+};
+
+export default RootLayout;
