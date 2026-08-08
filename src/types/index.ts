fb7f81ff346188afa1a58851ca0081b615235b4d
epickalevaphoto-cyber <epickaleva.photo@gmail.com>
2026-08-08 10:50:34 +0300

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,19 @@
-.
+export interface User {
+  id: string;
+  name: string;
+  role: 'player' | 'master';
+}
+
+export interface GameRoom {
+  id: string;
+  code: string;
+  name: string;
+  masterId: string;
+  players: User[];
+}
+
+export interface Character {
+  id: string;
+  name: string;
+  userId: string;
+}
