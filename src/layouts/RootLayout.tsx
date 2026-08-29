211a8813c14fa3aec605f8039ac1d28f80cea9b6
epickalevaphoto-cyber <epickaleva.photo@gmail.com>
2026-08-29 20:14:59 +0300

    Create RootLayout component for routing

---
+++ src/layouts/RootLayout.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router-dom';
+
+const RootLayout = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Outlet />
+    </div>
+  );
+};
+
+export default RootLayout;
